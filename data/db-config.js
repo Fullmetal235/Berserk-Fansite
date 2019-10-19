1034613cec1f499aa2bda0b5b33ac2c67510b63b
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-10-18 23:22:55 -0500

    updated dbConfig

--- data/db-config.js
+++ data/db-config.js
@@ -2,4 +2,4 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 const dbENV = process.env.db_ENV || 'development';
-module.exports = knex(knexConfig[dbENV]);  
+module.exports = knex(knexConfig.production);  
